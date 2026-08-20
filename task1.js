40246a845245dab0f1762d021d1235d3146c8da4
Satyajit Sahoo <sahoosatyajit916@gmail.in>
2026-08-21 01:33:33 +0530

    Add js codes for interactivity

---
+++ task1.js
@@ -0,0 +1,13 @@
+// Get the navigation bar
+const navbar = document.getElementById("navbar");
+
+// Detect scrolling
+window.addEventListener("scroll", function () {
+
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+
+});
